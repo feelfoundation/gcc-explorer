320df814c0aa6b8b4fdad80ff34e0e9b859a9d20
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:39:42 +0800

    Create transactions.js

---
+++ api/transactions.js
@@ -0,0 +1,46 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		path: 'getTransaction',
+		service: 'transactions',
+		params: req => req.query.transactionId,
+	}, {
+		path: 'getUnconfirmedTransactions',
+		service: 'transactions',
+		params: () => undefined,
+	}, {
+		path: 'getLastTransactions',
+		service: 'transactions',
+		params: () => undefined,
+	}, {
+		path: 'getTransactions',
+		service: 'transactions',
+		params: req => req.query,
+	}, {
+		path: 'getTransactionsByAddress',
+		service: 'transactions',
+		params: req => req.query,
+	}, {
+		path: 'getTransactionsByBlock',
+		service: 'transactions',
+		params: req => ({
+			blockId: req.query.blockId,
+			offset: req.query.offset,
+			limit: req.query.limit,
+		}),
+	},
+];
