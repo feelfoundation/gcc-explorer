c5985b7133fbefc67b3ca4644a6b889ba29e86ea
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:12:52 +0800

    Create os-icon.directive.js

---
+++ src/shared/peers/os-icon.directive.js
@@ -0,0 +1,38 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppTools from '../../app/app-tools.module';
+
+const osIcon = AppTools.directive('osIcon', () => ({
+	restrict: 'A',
+
+	scope: {
+		os: '=os',
+		brand: '=brand',
+	},
+
+	template: '<span></span>',
+	replace: true,
+
+	link(scope, element) {
+		const el = element[0];
+
+		el.alt = scope.os;
+		el.title = scope.os;
+		el.className += (` os-icon os-${scope.brand.name}`);
+	},
+}));
+
+export default osIcon;
