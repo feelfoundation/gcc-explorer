9cc6a470a273070c055c5ea8e9a1a306b9c9ab79
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:40:15 +0800

    Create delegates.js

---
+++ benchmarks/delegates.js
@@ -0,0 +1,45 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		endpoint: 'getActive',
+		service: 'delegates',
+		params: undefined,
+		data: data => data.delegates.length,
+	}, {
+		endpoint: 'getStandby',
+		service: 'delegates',
+		params: 0,
+		data: data => data.delegates.length,
+	}, {
+		endpoint: 'getLatestRegistrations',
+		service: 'delegates',
+		params: undefined,
+		title: 'registrations',
+		data: data => data.transactions.length,
+	}, {
+		endpoint: 'getLatestVotes',
+		service: 'delegates',
+		params: undefined,
+		title: 'votes',
+		data: data => data.transactions.length,
+	}, {
+		endpoint: 'getLastBlock',
+		service: 'delegates',
+		title: 'block',
+		params: undefined,
+	},
+];
