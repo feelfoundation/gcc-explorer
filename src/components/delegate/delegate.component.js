fc1abe00f523177249c2bdd044b541438f694588
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:01:47 +0800

    Create delegate.component.js

---
+++ src/components/delegate/delegate.component.js
@@ -0,0 +1,52 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppDelegate from './delegate.module';
+import template from './delegate.html';
+
+const DelegateConstructor = function ($rootScope, $stateParams,
+	$location, $http, $state) {
+	const vm = this;
+
+	$rootScope.breadCrumb = { address: $stateParams.delegateId };
+
+	vm.getAddress = () => {
+		$http.get('/api/getAccount', {
+			params: {
+				address: $stateParams.delegateId,
+			},
+		}).then((resp) => {
+			if (resp.data.success) {
+				vm.address = resp.data;
+
+				if (!vm.address.delegate) {
+					$state.go('address', { address: $stateParams.delegateId });
+				}
+			} else {
+				$state.go('home');
+			}
+		}).catch(() => {
+			$location.path('/');
+		});
+	};
+
+	vm.getAddress();
+};
+
+AppDelegate.component('delegate', {
+	template,
+	controller: DelegateConstructor,
+	controllerAs: 'vm',
+});
