74dfe413a6d11a8f5ee08b8a339f72be29b2b5fb
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:09:16 +0800

    Create index.js

---
+++ src/filters/index.js
@@ -0,0 +1,38 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import './filters.module';
+import './alter-word-separation';
+import './approval';
+import './currency';
+import './currencyFee';
+import './epoch-stamp';
+import './fiat';
+import './forging-time';
+import './feel';
+import './net-hash';
+import './round';
+import './split';
+import './start-from';
+import './supply-percent';
+import './time-ago';
+import './time-span';
+import './time-stamp';
+import './tx-recipient';
+import './tx-sender';
+import './tx-type';
+import './votes';
+import './middle-ellipsis';
+import './currency-code-to-name';
