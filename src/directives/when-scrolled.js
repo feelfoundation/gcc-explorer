2a671f9fb7c217980fa76dbf5c79a5a049bb04e4
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:08:13 +0800

    Create when-scrolled.js

---
+++ src/directives/when-scrolled.js
@@ -0,0 +1,42 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import angular from 'angular';
+import AppTools from '../app/app-tools.module';
+
+AppTools.directive('whenScrolled', $window => ({
+	restrict: 'A',
+
+	link(scope, elm, attr) {
+		let pageHeight;
+		let clientHeight;
+		let scrollPos;
+		$window = angular.element($window);
+
+		const handler = () => {
+			pageHeight = window.document.documentElement.scrollHeight;
+			clientHeight = window.document.documentElement.clientHeight;
+			scrollPos = window.pageYOffset;
+
+			if (pageHeight - (scrollPos + clientHeight) === 0) {
+				scope.$apply(attr.whenScrolled);
+			}
+		};
+
+		$window.on('scroll', handler);
+
+		scope.$on('$destroy', () => $window.off('scroll', handler));
+	},
+}));
