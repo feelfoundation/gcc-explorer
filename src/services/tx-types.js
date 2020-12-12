47b175ab5694a0e517d45e2432e72fb932e98432
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:12:07 +0800

    Create tx-types.js

---
+++ src/services/tx-types.js
@@ -0,0 +1,35 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppServices from './services.module';
+
+AppServices.value('txTypes', {
+	0: 'Balance transfer',
+	1: 'Second signature creation',
+	2: 'Delegate registration',
+	3: 'Delegate vote',
+	4: 'Multi-signature creation',
+	5: 'DApp registration',
+	6: 'DApp deposit',
+	7: 'DApp withdrawal',
+	8: 'Balance transfer',
+	9: 'Second signature creation',
+	10: 'Delegate registration',
+	11: 'Delegate vote',
+	12: 'Multi-signature creation',
+	13: 'DApp registration',
+	14: 'DApp deposit',
+	15: 'DApp withdrawal',
+});
