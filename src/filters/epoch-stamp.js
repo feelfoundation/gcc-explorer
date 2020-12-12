ad48e20fa30c8f7f98cfa4c16aa103399ff4acef
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:08:52 +0800

    Create epoch-stamp.js

---
+++ src/filters/epoch-stamp.js
@@ -0,0 +1,24 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppFilters from './filters.module';
+
+AppFilters.filter('epochStamp', $rootScope => (ts) => {
+	const epoch = $rootScope.nodeConstants && $rootScope.nodeConstants.epoch ?
+		$rootScope.nodeConstants.epoch :
+		'2016-05-24T17:00:00.000Z';
+
+	return (((new Date(epoch).getTime()) + ts) * 1000);
+});
