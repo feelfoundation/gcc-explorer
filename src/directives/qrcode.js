fc1b0c9c9e4012cf7290e073b6a4e259cc737107
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:08:03 +0800

    Create qrcode.js

---
+++ src/directives/qrcode.js
@@ -0,0 +1,36 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import qrcode from 'qrcode-generator';
+import AppTools from '../app/app-tools.module';
+
+AppTools.directive('qrcode', () => {
+	const QrcodeLinK = ($scope, $element, $attrs) => {
+		const typeNumber = 4;
+		const errorCorrectionLevel = 'L';
+		const qr = qrcode(typeNumber, errorCorrectionLevel);
+		qr.addData($attrs.data);
+		qr.make();
+		$element.html(qr.createSvgTag());
+		$element.find('svg').attr({
+			viewBox: '8 7 66 69',
+			width: '164',
+			height: '164',
+		});
+	};
+	return {
+		link: QrcodeLinK,
+	};
+});
