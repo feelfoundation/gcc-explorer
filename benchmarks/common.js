920b453a7d0df2990f1066629e3cce9cb4c3c0d2
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:40:11 +0800

    Create common.js

---
+++ benchmarks/common.js
@@ -0,0 +1,28 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		endpoint: 'getPriceTicker',
+		service: 'common',
+		title: 'price ticker',
+		params: undefined,
+	}, {
+		endpoint: 'search',
+		service: 'common',
+		title: 'price ticker',
+		params: '12907382053545086321C',
+	},
+];
