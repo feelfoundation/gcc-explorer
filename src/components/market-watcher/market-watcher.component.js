aa43b6f5523dacbcdc0ad81da7153372b304a14f
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:04:12 +0800

    Create market-watcher.component.js

---
+++ src/components/market-watcher/market-watcher.component.js
@@ -0,0 +1,28 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppMarketWatcher from './market-watcher.module';
+import template from './market-watcher.html';
+import './market-watcher.css';
+
+const MarketWatcherConstructor = function (marketWatcher) {
+	marketWatcher(this);
+};
+
+AppMarketWatcher.component('marketWatcher', {
+	template,
+	controller: MarketWatcherConstructor,
+	controllerAs: 'vm',
+});
