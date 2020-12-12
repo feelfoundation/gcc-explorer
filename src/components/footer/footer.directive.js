18bbe0cb2525b5600b7835d58ba307d8ecc6cbee
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:02:17 +0800

    Create footer.directive.js

---
+++ src/components/footer/footer.directive.js
@@ -0,0 +1,25 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import FooterApp from './footer.module';
+import template from './footer.html';
+
+const FooterLink = () => {};
+
+FooterApp.directive('mainFooter', () => ({
+	restrict: 'E',
+	link: FooterLink,
+	template,
+}));
