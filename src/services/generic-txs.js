aa27598ad9b39853022eb46e1990be13b628664c
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:11:05 +0800

    Create generic-txs.js

---
+++ src/services/generic-txs.js
@@ -0,0 +1,27 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppServices from './services.module';
+import Pagination from './pagination';
+
+AppServices.factory('genericTxs',
+	($http, $q) => (data) => {
+		const params = Object.assign({}, data, {
+			url: '/api/getTransactions',
+			key: 'transactions',
+		});
+
+		return new Pagination($http, $q, params);
+	});
