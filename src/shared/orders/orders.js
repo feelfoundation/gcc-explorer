c3f18ecfa1b660d4c5f5568f483a466641451917
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:12:33 +0800

    Create orders.js

---
+++ src/shared/orders/orders.js
@@ -0,0 +1,30 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppTools from '../../app/app-tools.module';
+import template from './orders.html';
+
+const orders = AppTools.directive('orders', () => ({
+	restrict: 'E',
+	scope: {
+		orders: '=orders',
+		heading: '@heading',
+		name: '@name',
+	},
+	template,
+	replace: true,
+}));
+
+export default orders;
