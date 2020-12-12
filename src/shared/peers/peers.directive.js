b59159377fa2be90171ff5ec1a62b99944a0b04d
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:12:57 +0800

    Create peers.directive.js

---
+++ src/shared/peers/peers.directive.js
@@ -0,0 +1,30 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppTools from '../../app/app-tools.module';
+import template from './peers.html';
+
+const peers = AppTools.directive('peers', orderBy => ({
+	restrict: 'E',
+	scope: { peers: '=' },
+	template,
+	replace: true,
+
+	link(scope) {
+		scope.table = orderBy('ip');
+	},
+}));
+
+export default peers;
