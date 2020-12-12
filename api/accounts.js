31f2c1dbe00d83d396999c3bcae202184d5f9272
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:35:03 +0800

    Create accounts.js

---
+++ api/accounts.js
@@ -0,0 +1,47 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		path: 'getAccount',
+		service: 'accounts',
+		params: req => req.query,
+	}, {
+		path: 'getTopAccounts',
+		service: 'accounts',
+		params: req => ({
+			offset: req.query.offset,
+			limit: req.query.limit,
+		}),
+	},
+	{
+		path: 'getVotes',
+		service: 'accounts',
+		params: req => ({
+			publicKey: req.query.publicKey,
+			offset: req.query.offset,
+			limit: req.query.limit,
+		}),
+	},
+	{
+		path: 'getVoters',
+		service: 'accounts',
+		params: req => ({
+			publicKey: req.query.publicKey,
+			offset: req.query.offset,
+			limit: req.query.limit,
+		}),
+	},
+];
