f3f4664d0bd137ad0ab912dc1cf08024a0e79fba
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:13:29 +0800

    Create transactions-list.directive.js

---
+++ src/shared/transactions-list/transactions-list.directive.js
@@ -0,0 +1,31 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+import AppTools from '../../app/app-tools.module';
+import template from './transactions-list.html';
+
+const transactionsList = AppTools.directive('transactionsList', orderBy => ({
+	template,
+	scope: {
+		txs: '=',
+		address: '=',
+	},
+	link: ($scope) => {
+		$scope.filter = orderBy('timestamp', true);
+	},
+}));
+
+export default transactionsList;
