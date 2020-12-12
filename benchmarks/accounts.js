110293d34a6171a8db3665f28c1185f357b5aa78
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:40:01 +0800

    Create accounts.js

---
+++ benchmarks/accounts.js
@@ -0,0 +1,27 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		endpoint: 'getAccount',
+		service: 'accounts',
+		params: { address: '12907382053545086321L' },
+	}, {
+		endpoint: 'getTopAccounts',
+		service: 'accounts',
+		params: { offset: 0, limit: 50 },
+		data: data => data.accounts.length,
+	},
+];
