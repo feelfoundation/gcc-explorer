5ed3e76a0eeb485876f05bf637b451a082b0724c
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:38:59 +0800

    Create blocks.js

---
+++ api/blocks.js
@@ -0,0 +1,34 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		path: 'getLastBlocks',
+		service: 'blocks',
+		params: req => req.query.n,
+	}, {
+		path: 'getBlock',
+		service: 'blocks',
+		params: req => req.query.blockId,
+	}, {
+		path: 'getHeight',
+		service: 'blocks',
+		params: req => req.query.height,
+	}, {
+		path: 'getBlockStatus',
+		service: 'blocks',
+		params: () => undefined,
+	},
+];
