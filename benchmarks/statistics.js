f1c6bd3ab2a12b27b52e290ea7dc3256969c1ee4
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:40:29 +0800

    Create statistics.js

---
+++ benchmarks/statistics.js
@@ -0,0 +1,35 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		endpoint: 'getLastBlock',
+		service: 'statistics',
+		title: 'blocks',
+		params: undefined,
+		data: data => data.volume.blocks,
+	}, {
+		endpoint: 'getBlocks',
+		service: 'statistics',
+		title: 'blocks',
+		params: undefined,
+	}, {
+		endpoint: 'getPeers',
+		service: 'statistics',
+		title: 'peers',
+		params: undefined,
+		data: data => (data.list.connected.length + data.list.disconnected.length),
+	},
+];
