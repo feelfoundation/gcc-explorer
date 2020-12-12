9a59fd4950443148014cd4063f60a6eaf984101f
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 11:59:18 +0800

    Create bread-crumb.module.js

---
+++ src/components/bread-crumb/bread-crumb.module.js
@@ -0,0 +1,20 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import angular from 'angular';
+
+const AppBreadCrumb = angular.module('feel_explorer.breadCrumb', []);
+
+export default AppBreadCrumb;
