958cd4a8b2245915a5d85e96140ce723efc9f28a
feelfoundation <feelfoundation@hotmail.com>
2020-12-12 12:12:01 +0800

    Create socket.js

---
+++ src/services/socket.js
@@ -0,0 +1,54 @@
+/*
+ * feelfoundation/gcc-explorer
+ * Copyright © 2018 Feel Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Feel Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import io from 'socket.io-client';
+import AppServices from './services.module';
+
+
+/**
+ * @todo isolate socket.io
+ */
+AppServices.factory('$socket',
+	($location, $rootScope) => (namespace) => {
+		const socket = io(`${$location.host()}:${$location.port()}${namespace}`, { forceNew: true });
+
+		return {
+			on(eventName, callback) {
+				socket.on(eventName, (...args) => {
+					$rootScope.$apply(() => {
+						callback.apply(socket, args);
+					});
+				});
+			},
+
+			emit(eventName, data, callback) {
+				socket.emit(eventName, data, (...args) => {
+					$rootScope.$apply(() => {
+						if (callback) {
+							callback.apply(socket, args);
+						}
+					});
+				});
+			},
+
+			removeAllListeners(eventName, callback) {
+				socket.removeAllListeners(eventName, (...args) => {
+					$rootScope.$apply(() => {
+						callback.apply(socket, args);
+					});
+				});
+			},
+		};
+	});
